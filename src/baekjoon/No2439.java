6e066ba73c98a4c844e8ffd4579235e643f7309c
Myeongjin <mangs2e@naver.com>
2023-06-18 17:16:34 +0900

    별찍기

---
+++ src/baekjoon/No2439.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class No2439 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        for(int i=1;i<=N;i++) {
+            for(int j=N-i;j>=1;j--) {
+                System.out.print(' ');
+            }
+            for(int j=1;j<=i;j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
