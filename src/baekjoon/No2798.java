2d49de2f574a707c5813260f74b869ef05da0ba6
Myeongjin <mangs2e@naver.com>
2023-06-19 16:04:18 +0900

    브루트 포스 - 블랙잭

---
+++ src/baekjoon/No2798.java
@@ -0,0 +1,41 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class No2798 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        List<Integer> sumList = new ArrayList<>();
+        int sum = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0;i<N;i++) {
+            for(int j=i+1;j<N;j++) {
+                for(int k=j+1;k<N;k++) {
+                    sum = arr[i] + arr[j] + arr[k];
+                    if(!(sum > M)) {
+                        sumList.add(sum);
+                    }
+                }
+            }
+        }
+
+        int max = Collections.max(sumList);
+        System.out.println(max);
+    }
+}
