2355d9de6c40d0ee9438e82f574439bd4d65beb1
Myeongjin <mangs2e@naver.com>
2023-06-19 11:42:47 +0900

    수학 공식 사용하여 피라미드 둘레 구하기

---
+++ src/baekjoon/No15894.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No15894 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Integer.parseInt(br.readLine());
+
+        System.out.println(4*n);
+    }
+}
