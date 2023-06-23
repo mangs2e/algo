35eac1ac471782880e98ce18928428c2d3c810d2
Myeongjin <mangs2e@naver.com>
2023-06-23 09:31:49 +0900

    브루트 포스 - 단어 정렬

---
+++ src/baekjoon/brute_force/No1181.java
@@ -0,0 +1,26 @@
+package baekjoon.brute_force;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class No1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<String, Integer> map = new TreeMap<>();
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0;i<N;i++) {
+            String str = br.readLine();
+            map.put(str, str.length());
+        }
+        List<Map.Entry<String, Integer>> entryList = new LinkedList<>(map.entrySet());
+        entryList.sort(Map.Entry.comparingByValue());
+
+        for (Map.Entry<String, Integer> entry : entryList) {
+            System.out.println(entry.getKey());
+        }
+
+    }
+}
