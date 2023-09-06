9f6e6935cb29294be3cb10069f49dbe0b2335a14
Myeongjin <mangs2e@naver.com>
2023-09-06 11:43:40 +0900

    동적 계획법 - 1로 만들기

---
+++ src/baekjoon/dp/No1463.java
@@ -0,0 +1,27 @@
+package baekjoon.dp;
+
+import java.util.Scanner;
+
+public class No1463 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            if(dp[i] == 0) {
+                if(i % 6 == 0) dp[i] = Math.min(dp[i - 1], Math.min(dp[i / 2], dp[i / 3])) + 1;
+                else if(i % 2 == 0) dp[i] = Math.min(dp[i - 1], dp[i / 2]) + 1;
+                else if(i % 3 == 0) dp[i] = Math.min(dp[i - 1], dp[i / 3]) + 1;
+                else dp[i] = dp[i - 1] + 1;
+            }
+        }
+
+        System.out.println(dp[n]);
+
+    }
+}
