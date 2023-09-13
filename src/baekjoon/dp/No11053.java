498d0a98f1f05d176c78b4ba5f0afcd67c7e0b9c
Myeongjin <mangs2e@naver.com>
2023-09-13 09:45:28 +0900

    동적 계획법 - 가장 긴 증가하는 부분 수열

---
+++ src/baekjoon/dp/No11053.java
@@ -0,0 +1,30 @@
+package baekjoon.dp;
+
+import java.util.Scanner;
+
+public class No11053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+        int max = 1;
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if(arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
