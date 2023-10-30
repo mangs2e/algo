d00a9cef8ed424e6682becb55e7e5f77cf044b07
Myeongjin <mangs2e@naver.com>
2023-10-30 13:36:17 +0900

    dp - Four squares

---
+++ src/baekjoon/dp/No17626.java
@@ -0,0 +1,27 @@
+package baekjoon.dp;
+
+import java.util.Scanner;
+
+public class No17626 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] dp = new int[n+1];
+
+        dp[1] = 1;
+
+        int min;
+        for (int i = 2; i <= n; i++) {
+            min = Integer.MAX_VALUE;
+
+            for (int j = 1; j * j <= i; j++) {
+                int temp = i - j * j;
+                min = Math.min(min, dp[temp]);
+            }
+
+            dp[i] = min + 1;
+        }
+        System.out.println(dp[n]);
+    }
+}
