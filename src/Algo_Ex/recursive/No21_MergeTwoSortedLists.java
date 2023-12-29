3538146ba4112cd27fb045fd6756f80e3cd4432f
Myeongjin <mangs2e@naver.com>
2023-12-29 13:06:01 +0900

    재귀 - Single Linked list 두개를 병합해서 정렬하는 알고리즘 (재귀는 어려워..)

---
+++ src/Algo_Ex/recursive/No21_MergeTwoSortedLists.java
@@ -0,0 +1,40 @@
+package Algo_Ex.recursive;
+
+public class No21_MergeTwoSortedLists {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode result = mergeTwoLists(list1, list2);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
