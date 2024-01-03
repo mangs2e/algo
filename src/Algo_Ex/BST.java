5615d30434d9201a521d12cbe93026fc8fbba0b5
Myeongjin <mangs2e@naver.com>
2024-01-03 12:31:34 +0900

    Binary Search Tree - insert, delete

--- src/Algo_Ex/BST.java
+++ src/Algo_Ex/BST.java
@@ -79,7 +79,7 @@ class BinarySearchTree {
     }
 }
 
-public class BTS {
+public class BST {
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
         /*
