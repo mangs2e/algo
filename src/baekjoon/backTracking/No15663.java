00b3184f9c7c4483d4e6903de0486b39af6e6dbf
Myeongjin <mangs2e@naver.com>
2024-01-08 16:42:03 +0900

    백트래킹 - 한번 만든 순열을 다시 만들지 않도록! 다만 중복값 하나하나는 있을 수 있음 (내가 짠 코드)

---
+++ src/baekjoon/backTracking/No15663.java
@@ -0,0 +1,52 @@
+package baekjoon.backTracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class No15663 {
+    static int[] arr;
+    static Set<String> set = new HashSet<>();
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        combine(new ArrayList<>(), M);
+        System.out.println(sb);
+    }
+
+    private static void combine(ArrayList<Integer> list, int m) {
+        if (m == 0) {
+            StringBuilder str = new StringBuilder();
+            for (int k : list) {
+                str.append(arr[k]).append(" ");
+            }
+            if (!set.contains(str.toString())) {
+                set.add(str.toString());
+                sb.append(str).append("\n");
+            }
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (list.contains(i)) {
+                continue;
+            }
+            list.add(i);
+            combine(list, m - 1);
+            list.remove(list.size() - 1);
+        }
+    }
+}
